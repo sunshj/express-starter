d5d6be33ff28b85148c87451c4df35cf1dfdc7e0
sunshj <sunshj317@qq.com>
2024-03-16 16:16:28 +0800

    chore: 升级依赖，使用同步方法注册路由

--- src/app.js
+++ src/app.js
@@ -3,51 +3,47 @@ const express = require('express')
 const favicon = require('serve-favicon')
 const createError = require('http-errors')
 const path = require('path')
-const { setupRouter } = require('express-filebased-routing')
+const { setupRouterSync } = require('express-filebased-routing')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const { applyMiddlewares, errorHandler } = require('./middlewares')
 
 const PORT = process.env.PORT || 3000
 
-async function main() {
-    const app = express()
+const app = express()
 
-    applyMiddlewares(app)
+applyMiddlewares(app)
 
-    // favicon
-    app.use(favicon(path.join(process.cwd(), '/public', 'favicon.ico')))
+// favicon
+app.use(favicon(path.join(process.cwd(), '/public', 'favicon.ico')))
 
-    // view engine setup
-    app.set('views', path.join(__dirname, 'views'))
-    app.set('view engine', 'ejs')
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-    app.use(logger('dev'))
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: false }))
-    app.use(cookieParser())
-    app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-    // routes auto generate
-    await setupRouter(app, {
-        directory: path.join(__dirname),
-        logger: {
-            enable: true,
-            baseUrl: 'http://127.0.0.1:3500',
-        },
-    })
+// routes auto generate
+setupRouterSync(app, {
+    directory: path.join(__dirname),
+    logger: {
+        enable: true,
+        baseUrl: 'http://127.0.0.1:3500',
+    },
+})
 
-    // catch 404 and forward to error handler
-    app.use(function (req, res, next) {
-        next(createError(404))
-    })
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404))
+})
 
-    // error handler
-    app.use(errorHandler)
+// error handler
+app.use(errorHandler)
 
-    app.listen(PORT, () => {
-        console.log(`➜  Local:   http://localhost:${PORT}`)
-    })
-}
-
-main()
+app.listen(PORT, () => {
+    console.log(`➜  Local:   http://localhost:${PORT}`)
+})
