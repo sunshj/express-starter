3a316b6c9d49be3f063bc051fc49d020487ebd45
sunshj <sunshj317@qq.com>
2024-07-13 00:48:33 +0800

    chore: bump deps

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -2,7 +2,7 @@ import { createJoiValidator, extractJoiError } from 'express-validate-kit'
 
 export const joiValidator = createJoiValidator({
   statusCode: 200,
-  errorHandler(error) {
+  errorResponse(error) {
     return {
       code: 400,
       message: extractJoiError(error)
